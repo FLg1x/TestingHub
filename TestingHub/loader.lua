818296b8312e919bd081299f8c4f3031bb4b114b
FLg1x <4mir4ik@gmail.com>
2026-07-16 16:52:35 +0500

    Delete TestingHub/loader.lua

--- TestingHub/loader.lua
+++
@@ -1,7 +0,0 @@
-local BASE_URL = "https://raw.githubusercontent.com/ТВОЙ_НИК/НАЗВАНИЕ_РЕПОЗИТОРИЯ/main/FokiHub/modules/"
-
-local aimbot = loadstring(game:HttpGet(BASE_URL .. "aimbot.lua"))()
-local esp = loadstring(game:HttpGet(BASE_URL .. "esp.lua"))()
-local movement = loadstring(game:HttpGet(BASE_URL .. "movement.lua"))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ТВОЙ_НИК/НАЗВАНИЕ_РЕПОЗИТОРИЯ/main/FokiHub/main.lua"))()
